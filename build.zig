a3c1d944180a5cbddc54a0c19e3fb5de2ecc645b
Diego Covarrubias <dcov@pm.me>
2023-07-03 10:37:58 -0700

    add module fn to build.zig

--- build.zig
+++ build.zig
@@ -2,14 +2,34 @@ const std = @import("std");
 const vkgen = @import("libs/vulkan-zig/generator/index.zig");
 const ftgen = @import("libs/mach-freetype/build.zig");
 
-pub fn build(b: *std.Build) !void {
-    const vulkan = try vulkanModule(b);
-    const freetype = ftgen.module(b);
-    const harfbuzz = ftgen.harfbuzzModule(b);
-    const known_folders = b.createModule(.{
-        .source_file = .{ .path = "libs/known-folders/known-folders.zig" },
-    });
+const src_root = std.fs.path.dirname(@src().file) orelse ".";
 
+pub fn module(b: *std.Build) !*std.Build.Module {
+    const deps = try makeDependencies(b);
+    return b.createModule(.{
+        .source_file = .{ .path = src_root ++ "src/lib.zig" },
+        .dependencies = &.{
+            .{
+                .name = "vulkan",
+                .module = deps.vulkan,
+            },
+            .{
+                .name = "freetype",
+                .module = deps.freetype,
+            },
+            .{
+                .name = "harfbuzz",
+                .module = deps.harfbuzz,
+            },
+            .{
+                .name = "known_folders",
+                .module = deps.known_folders,
+            },
+        },
+    });
+}
+
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const lib_tests = b.addTest(.{
@@ -18,15 +38,32 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
     });
 
-    lib_tests.addModule("vulkan", vulkan);
-    lib_tests.addModule("freetype", freetype);
-    lib_tests.addModule("harfbuzz", harfbuzz);
-    lib_tests.addModule("known_folders", known_folders);
+    const deps = try makeDependencies(b);
+    lib_tests.addModule("vulkan", deps.vulkan);
+    lib_tests.addModule("freetype", deps.freetype);
+    lib_tests.addModule("harfbuzz", deps.harfbuzz);
+    lib_tests.addModule("known_folders", deps.known_folders);
     ftgen.link(b, lib_tests, .{ .harfbuzz = .{} });
 
     const run_tests = b.addRunArtifact(lib_tests);
     const run_tests_step = b.step("test", "Run tests");
     run_tests_step.dependOn(&run_tests.step);
+}
+
+fn makeDependencies(b: *std.Build) !struct {
+    vulkan: *std.Build.Module,
+    freetype: *std.Build.Module,
+    harfbuzz: *std.Build.Module,
+    known_folders: *std.Build.Module,
+} {
+    return .{
+        .vulkan = try vulkanModule(b),
+        .freetype = ftgen.module(b),
+        .harfbuzz = ftgen.harfbuzzModule(b),
+        .known_folders = b.createModule(.{
+            .source_file = .{ .path = "libs/known-folders/known-folders.zig" },
+        }),
+    };
 }
 
 fn vulkanModule(b: *std.Build) !*std.Build.Module {
@@ -42,9 +79,8 @@ fn vulkanModule(b: *std.Build) !*std.Build.Module {
 
 fn ensureCachedFile(b: *std.Build, path: []const u8, url: []const u8) !void {
     const file = std.fs.openFileAbsolute(path, .{}) catch |e| {
-        const Error = std.fs.File.OpenError;
         switch (e) {
-            Error.FileNotFound => {
+            error.FileNotFound => {
                 _ = b.exec(&.{ "curl", url, "-o", path });
                 return;
             },
