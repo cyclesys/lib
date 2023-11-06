047191ab822fb2905ff5279083609cd7c4ab2391
Diego Covarrubias <dcov@pm.me>
2023-11-05 17:33:20 -0800

    update lib exports

--- src/lib.zig
+++ src/lib.zig
@@ -1,3 +1,9 @@
+pub const chan = struct {
+    pub usingnamespace @import("chan/SharedMem.zig");
+    pub usingnamespace @import("chan/channel.zig");
+    pub usingnamespace @import("chan/serde.zig");
+};
+
 pub const def = struct {
     pub const ObjectScheme = @import("def/ObjectScheme.zig");
     pub usingnamespace @import("def/define.zig");
@@ -5,15 +11,26 @@ pub const def = struct {
     pub usingnamespace @import("def/type.zig");
 };
 
-pub const chan = struct {
-    pub usingnamespace @import("chan/SharedMem.zig");
-    pub usingnamespace @import("chan/channel.zig");
-    pub usingnamespace @import("chan/serde.zig");
+const init_mod = @import("init.zig");
+pub const SystemMessage = init_mod.SystemMessage;
+pub const PluginMessage = init_mod.PluginMessage;
+pub const InitConfig = init_mod.InitConfig;
+pub const init = init_mod.init;
+
+pub const obj = struct {
+    pub usingnamespace @import("obj/index.zig");
+
+    const serde = @import("obj/serde.zig");
+    pub const UpdateObject = serde.UpdateObject;
+
+    pub usingnamespace @import("obj/write.zig");
 };
 
-pub const main = @import("main.zig");
-pub const InitConfig = main.InitConfig;
-pub const init = main.init;
+pub const ui = struct {
+    pub const render = struct {
+        pub const vulkanLoader = @import("ui/render/Context.zig").vulkanLoader;
+    };
+};
 
 test {
     _ = @import("def/ObjectScheme.zig");
@@ -21,4 +38,9 @@ test {
     _ = @import("chan/channel.zig");
     _ = @import("chan/serde.zig");
     _ = @import("obj/write.zig");
+    _ = @import("ui/text/bidi.zig");
+    _ = @import("ui/text/GlyphAtlas.zig");
+    _ = @import("ui/text/GraphemeBreak.zig");
+    _ = @import("ui/text/LineBreak.zig");
+    _ = @import("ui/text/WordBreak.zig");
 }
