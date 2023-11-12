e976138dbf15183047ad2999a60fb6d939f23c16
Diego Covarrubias <dcov@pm.me>
2023-11-11 17:21:51 -0800

    move init.zig functionality into root lib.zig module

--- src/lib.zig
+++ src/lib.zig
@@ -1,3 +1,6 @@
+const std = @import("std");
+const win = @import("windows.zig");
+
 pub const chan = struct {
     pub usingnamespace @import("chan/SharedMem.zig");
     pub usingnamespace @import("chan/channel.zig");
@@ -11,12 +14,6 @@ pub const def = struct {
     pub usingnamespace @import("def/type.zig");
 };
 
-const init_mod = @import("init.zig");
-pub const SystemMessage = init_mod.SystemMessage;
-pub const PluginMessage = init_mod.PluginMessage;
-pub const InitConfig = init_mod.InitConfig;
-pub const init = init_mod.init;
-
 pub const obj = struct {
     pub usingnamespace @import("obj/index.zig");
     pub usingnamespace @import("obj/serde.zig");
@@ -28,6 +25,105 @@ pub const ui = struct {
     pub const render = struct {
         pub const vulkanLoader = @import("ui/render/Context.zig").vulkanLoader;
     };
+};
+
+pub const SystemMessage = union(enum) {
+    AddObject: struct {
+        id: u128,
+        bytes: []const u8,
+    },
+    UpdateObject: struct {
+        id: u128,
+        bytes: []const u8,
+    },
+    RemoveObject: struct {
+        id: u128,
+    },
+};
+
+pub const PluginMessage = union(enum) {
+    SetVersion: struct {
+        major: u16,
+        minor: u16,
+    },
+    SetIndex: []const def.ObjectScheme,
+    Finalize: void,
+};
+
+pub fn init(allocator: std.mem.Allocator, cfg: struct {
+    index: []const def.ObjectScheme,
+}) !struct {
+    chan.Reader(SystemMessage),
+    chan.Writer(PluginMessage),
+} {
+    const cmd_line = try CommandLine.init(allocator);
+    defer cmd_line.deinit();
+
+    const reader, var writer = try CommandLine.read(allocator);
+
+    try writer.write(.{
+        .SetVersion = .{
+            .major = 0,
+            .minor = 1,
+        },
+    });
+
+    try writer.write(.{
+        .SetIndex = cfg.index,
+    });
+
+    try writer.write(.Finalize);
+
+    return .{
+        reader,
+        writer,
+    };
+}
+
+const CommandLine = struct {
+    args: [][:0]u8,
+    pos: usize,
+
+    fn read(allocator: std.mem.Allocator) !struct {
+        chan.Reader(SystemMessage),
+        chan.Writer(PluginMessage),
+    } {
+        const args = try std.process.argsAlloc(allocator);
+        defer std.process.argsFree(allocator, args);
+
+        var cmd_line = CommandLine{
+            .args = args,
+            .pos = 1,
+        };
+
+        return .{
+            chan.Reader(SystemMessage).init(
+                allocator,
+                try chan.Channel.import(
+                    try cmd_line.next(),
+                    try cmd_line.next(),
+                    try cmd_line.next(),
+                ),
+            ),
+            chan.Writer(PluginMessage).init(
+                allocator,
+                try chan.Channel.import(
+                    try cmd_line.next(),
+                    try cmd_line.next(),
+                    try cmd_line.next(),
+                ),
+            ),
+        };
+    }
+
+    fn next(self: *@This()) !win.HANDLE {
+        if (self.pos >= self.args.len) {
+            return error.InvalidArgs;
+        }
+        const int_ptr = try std.fmt.parseUnsigned(usize, self.args[self.pos], 16);
+        self.pos += 1;
+        return @ptrFromInt(int_ptr);
+    }
 };
 
 test {
